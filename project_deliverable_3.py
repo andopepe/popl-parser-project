5bb7a8892482c3dfd251e47e7c27a3bad4a263b9
Ando Pepe <coolcatando@gmail.com>
2024-12-02 17:53:37 -0600

    delevirables

--- project_deliverable_3.py
+++ project_deliverable_3.py
@@ -6,6 +6,7 @@ assign4 = assign1
 arith_op1 = 1 + 2
 arith_op2 = 13 - 3
 arith_op3 = 10 / arith_op1
+# Deliverable 1
 arith_op4 = 4.2 * 10
 arith_op5 = arith_op1 % arith_op2
 
@@ -27,6 +28,8 @@ flag = True
 
 assign1 = ""
 
+# Deliverable 2
+
 if var1 > var2:
 	arith_op1 = 1 + 2
 	assign1 = "text data"
@@ -46,3 +49,37 @@ elif (not flag) and var3 > 10:
 	flag = False
 else:
 	data = -1
+
+# Deliverable 3
+
+## comment test 1
+
+''' 
+	comment test 2
+	adding more comments
+	and more...
+'''
+
+while data > 0 or data != 0:
+	data = data - 1
+	if True:
+		a = "This is the weirdest code I have ever written"
+
+for data in array1:
+	if data < 0:
+		while(data != 0):
+			b = "This code doesn't make any sense"
+			data = data + 1
+	elif data > 0:
+		while(data != 0):
+			c = "I feel like I have no purpose..."
+			data = data - 1
+
+for i in range(0,5):
+	data = data * 2
+	data = data - 1
+
+''' I need help, this code shouldn't even exist '''
+while True:
+	data = 30
+	data = data - 1
